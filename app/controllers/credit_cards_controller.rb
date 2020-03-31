bebceca2a084bf040d6f01bc41e5224d0cca4098
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-03-31 18:00:43 +0900

    Create credit_cards_controller for all 7 actions
    ・Credit_cardコントローラーを作成
    ・7つのactionと対応するviewを生成

---
+++ app/controllers/credit_cards_controller.rb
@@ -0,0 +1,22 @@
+class CreditCardsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def new
+  end
+end
