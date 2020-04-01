d8c3acce003b4bbdbd448cf5456b73aa190a6b39
kashimaraiki <raiki3290@gmail.com>
2020-04-01 19:15:06 +0900

    アソシエーションの定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :items
+  has_many :comments
+  has_many :favorites
+  has_one :credit_card
+  has_one :address
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
