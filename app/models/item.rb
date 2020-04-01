f962e418dff5d852d1bfb3f624eba3d26247fd22
kashimaraiki <raiki3290@gmail.com>
2020-04-01 19:19:24 +0900

    コロンの付け忘れ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many :favorites
-  has_many :comments throught: :comments
+  has_many :comments, throught: :comments
   has_many :item_images
   belongs_to :user
 end
