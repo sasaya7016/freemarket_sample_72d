e115aeb4136f6d2cc9cff889b3a380738743f67c
kashimaraiki <raiki3290@gmail.com>
2020-04-01 20:05:19 +0900

    レビュー後の訂正
    アソシエーションの無駄な記述の削除と、has_many :commentsにthrought: :commentsを追加。そしてREADMEにも変更点を追加。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :items
-  has_many :comments
+  has_many :comments, through: :comments
   has_many :favorites
   has_one :credit_card
   has_one :address
