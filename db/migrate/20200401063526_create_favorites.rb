017b0d10a9a88fc448ee2f2bec66e03f8a6abc50
kashimaraiki <raiki3290@gmail.com>
2020-04-01 16:58:21 +0900

    user、credit_card、favoriteテーブルの完成

--- db/migrate/20200401063526_create_favorites.rb
+++ db/migrate/20200401063526_create_favorites.rb
@@ -1,8 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-      t.reference :user_id, null:false, foreign_key: true
-      t.reference :item_id, null:false, foreign_key: true
+      t.integer :user_id, null:false, foreign_key: true
+      t.integer :item_id, null:false, foreign_key: true
       t.timestamps
     end
   end
