e593e581b4baa583be03cfde2a8746fbeeaa464f
kandori-31 <ezynmthd@i.softbank.jp>
2020-04-03 16:00:32 +0900

    データ及び前後の商品の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: [:edit, :destroy]
+
+  def move_to_index
+    if !user_signed_in?
+      redirect_to root_path
+    elsif current_user.id!=exhibitor_id
+        redirect_to root_path
+      end
+  end
+  
   def index
   end
 
   def show
+    @item = Item.find(params[:id])
+    @user = User.where(id: @item.exhibitor.id).first
+    @address = Address.where(id: @user.id).first
   end
+
 
   def create
   end
@@ -19,4 +33,5 @@ class ItemsController < ApplicationController
 
   def new
   end
+
 end
