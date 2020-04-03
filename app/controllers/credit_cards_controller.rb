68c3eba5a121e2c2ead96512bd109f8aef4b90b3
koo1129 <koo.1192icloud.com>
2020-04-03 20:26:10 +0900

    仮

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,8 +1,20 @@
 class CreditCardsController < ApplicationController
+
+  require 'payjp'
+
   def index
   end
 
   def show
+    credit_card = CreditCard.where(user_id: current_user.id).first
+    if credit_card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = ENV["sk_test_1e3fb5db965cfd4b05705ae9"]
+      customer = Payjp::Customer.retrieve(credit_card.customer_id)
+      @default_card_information = customer.credit_cards.retrieve(credit_card.card_id)
+    end
+  end
   end
 
   def create
@@ -18,5 +30,38 @@ class CreditCardsController < ApplicationController
   end
 
   def new
+    credit_card = CreditCard.where(user_id: current_user.id)
+    redirect_to credit_card_path(current_user.id).empty?
   end
-end
+
+  def pay #payjpとCardのデータベース作成を実施します。
+    Payjp.api_key = ENV["sk_test_1e3fb5db965cfd4b05705ae9"]
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(
+      credit_card: params['payjp-token'],
+      metadata: {user_id: current_user.id}
+      ) #念の為metadataにuser_idを入れましたがなくてもOK
+      @credit_card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, credit_card_id: customer.default_card)
+      if @credit_card.save
+        redirect_to action: "show"
+      else
+        redirect_to action: "pay"
+      end
+    end
+  end
+
+  def delete #PayjpとCardデータベースを削除します
+    credit_card = CreditCard.where(user_id: current_user.id).first
+    if credit_card.blank?
+    else
+      Payjp.api_key = ENV["sk_test_1e3fb5db965cfd4b05705ae9"]
+      customer = Payjp::Customer.retrieve(credit_card.customer_id)
+      customer.delete
+      credit_card.delete
+    end
+      redirect_to action: "new"
+  end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#index'
+  root 'items#buy'
   resources :users
   resources :items
-  resources :credit_cards
+  resources :credit_cards do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
