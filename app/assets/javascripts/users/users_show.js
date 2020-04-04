d80bc799ba13c72f55fd0819b20d7a2150958f41
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-04 18:20:54 +0900

    Update users_show.js

--- app/assets/javascripts/users/users_show.js
+++ app/assets/javascripts/users/users_show.js
@@ -1,5 +1,9 @@
 
-let mypageTab = document.getElementById("mypage-tab__list");
-mypageTab.addEventListener('click',()=>{
+let mypageTabNewSells = document.getElementById("mypage-tab__list-new");
+mypageTabNewSells.addEventListener('click',()=>{
+  let activeTab = document.getElementById("tab__content-new");
+  let inactiveTab = document.getElementById("tab__content-old");
+  mypageTabNewSells.parentNode.classList.add("active-tab");
+  inactiveTab.classList.add("display--none")
 
 });
