4fbe3c4be352caf961707f9f2b07cd1745f1a394
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-05 11:58:14 +0900

    Update routes.rb
    ・supportアクションの下にcontact, withdrawのアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :users
+  resources :users do
+    collection do
+      get :support
+      scope :support do
+          get :contact
+          get :withdraw
+      end
+    end
+  end
   resources :items
   resources :credit_cards
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
