9c8109be7d071aadb384cbb92c3b58002e32c399
kandori-31 <ezynmthd@i.softbank.jp>
2020-04-06 16:51:08 +0900

    beforeアクションなどの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,44 @@
 class ItemsController < ApplicationController
-  before_action :set_item 
+  before_action :set_item , only: [:show, :buy, :edit, :destroy]
   before_action :move_to_index, only: [:edit, :destroy]
   before_action :not_buy, only: [:buy]
 
+  
+  def index
+  end
+  
+  def show
+    @user = User.where(id: @item.exhibitor.id).first
+    @address = Address.where(id: @user.id).first
+  end
+  
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def destroy
+  end
+  
+  def update
+  end
+  
+  def new
+  end
+
+  private
   def set_item
     @item = Item.find(params[:id])
   end
-
+  
   def not_buy
     if current_user.id == @item.exhibitor_id
       redirect_to root_path
     end
     end
-
+  
   def move_to_index
     if !user_signed_in?
       redirect_to root_path
@@ -20,30 +46,4 @@ class ItemsController < ApplicationController
         redirect_to root_path
       end
   end
-  
-  def index
-  end
-
-  def show
-    @item = Item.find(params[:id])
-    @user = User.where(id: @item.exhibitor.id).first
-    @address = Address.where(id: @user.id).first
-  end
-
-
-  def create
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
-  def update
-  end
-
-  def new
-  end
-
 end
