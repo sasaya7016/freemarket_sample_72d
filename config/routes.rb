cd4fa6d771345d4ada2644c986bb6ca3fde4f695
koo1129 <koo.1192icloud.com>
2020-04-06 13:20:39 +0900

    お問い合わせホーム

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,14 @@ Rails.application.routes.draw do
           get :contact
           get :withdraw
           get :adress
+          get :registration_enquiry
+          get :defect_enquiry
+          get :trouble_enquiry
+          get :request_enquiry
+          get :evaluation_enquiry
+          get :buy_enquiry
+          get :withdraw_enquiry
+          get :other_enquiry
       end
     end
   end
