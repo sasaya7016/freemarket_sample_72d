55b259857dd0d28e05555816c22132b2adf186d5
kandori-31 <ezynmthd@i.softbank.jp>
2020-04-06 15:03:00 +0900

    購入リンク作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :set_item 
   before_action :move_to_index, only: [:edit, :destroy]
+  before_action :not_buy, only: [:buy]
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def not_buy
+    if current_user.id == @item.exhibitor_id
+      redirect_to root_path
+    end
+    end
 
   def move_to_index
     if !user_signed_in?
       redirect_to root_path
-    elsif current_user.id!=exhibitor_id
+    elsif current_user.id!=@item.exhibitor_id
         redirect_to root_path
       end
   end
