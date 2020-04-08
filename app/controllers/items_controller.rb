84c9f033fb293184cea15bd04a68c9759147a5c5
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-08 10:47:15 +0900

    インデント調整とストロングパラメータ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,12 +37,22 @@ class ItemsController < ApplicationController
   def get_item_size
     selected_grandchild = Category.find("#{params[:grandchild_id]}")
     if related_size_parent = selected_grandchild.item_sizes[0]
-       @item_sizes = related_size_parent.children
-    else
-       selected_child = Category.find("#{params[:grandchild_id]}").parent
-       if related_size_parent = selected_child.item_sizes[0]
-          @item_sizes = related_size_parent.children
-       end
-    end
- end
+        @item_sizes = related_size_parent.children
+      else
+        selected_child = Category.find("#{params[:grandchild_id]}").parent
+        if related_size_parent = selected_child.item_sizes[0]
+            @item_sizes = related_size_parent.children
+        end
+      end
+  end
+
+  private
+
+  def item_params
+    #ItemModelでインクルードしたモジュールメソッドを使う(他のモデルで流用可能)
+    reject = %w(category_id ,buyer_id)
+    columns = Item.column_symbolized_names(reject).push(category_id: []) #category_idを配列で追加
+    params.require(:item).permit(*columns)
+  end
+
 end
