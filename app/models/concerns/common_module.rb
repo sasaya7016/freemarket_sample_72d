cb14605665a4a387e2d0f0e5a450b558acce2909
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-08 10:44:56 +0900

    カラム名をシンボルで取るモジュールの追加

---
+++ app/models/concerns/common_module.rb
@@ -0,0 +1,10 @@
+module CommomModule
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    #カラム名のシンボル型を配列で定義
+    def column_symbolized_names(reject=[])
+      column_names.delete_if{|n| n.in?(reject)}.map(&:to_sym)
+    end
+  end
+end
