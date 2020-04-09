637c700bd7ca39b7f5f159990652f655760caa20
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-09 19:19:21 +0900

    UserモデルにOptional: trueを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   has_many :favorites
   has_many :comments
   has_many :item_images
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :category
   
   #ActiveHashのアソシエーション
