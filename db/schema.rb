ef2d42aabe2d9471a60291115e11e8a29c95360a
mitsumakiNT <mitumakifr@gmail.com>
2020-04-09 18:40:03 +0900

    Get_ready_Rspec

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2020_04_05_005200) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-
     t.integer "postalcode", null: false
     t.integer "prefecture_id", null: false
     t.string "first_address", null: false
@@ -21,10 +20,24 @@ ActiveRecord::Schema.define(version: 2020_04_05_005200) do
     t.string "third_address"
     t.integer "phone_number"
     t.bigint "user_id"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "category_sizes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "item_size_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -71,11 +84,10 @@ ActiveRecord::Schema.define(version: 2020_04_05_005200) do
     t.text "introduction", null: false
     t.integer "price", null: false
     t.string "brand"
-
     t.integer "category_id", null: false
     t.string "item_size"
     t.string "status", null: false
-    t.string "preparation_day", null: false
+    t.integer "preparation_day", null: false
     t.integer "exhibitor_id"
     t.integer "buyer_id"
     t.string "delivery_fee", null: false
@@ -100,7 +112,6 @@ ActiveRecord::Schema.define(version: 2020_04_05_005200) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "prefecture_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "nicknameがない場合は登録できない" do
+      
+    end
+  end
+end
