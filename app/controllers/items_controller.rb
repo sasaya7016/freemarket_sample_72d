0fa815e48eb18a7f8b58c68c0064eaadaf543292
kandori-31 <ezynmthd@i.softbank.jp>
2020-04-09 10:32:00 +0900

    モデル　単体テスト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,52 +1,41 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
   
   require "payjp"
 
-
   def buy #クレジット購入
-    @item = Item.find(params[:id])
-    card = CreditCard.where(user_id: current_user.id ).first
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    customer = Payjp::Customer.retrieve(card.customer_id)
-    @default_card_information = customer.cards.retrieve(card.card_id)
+    card = CreditCard.where(user_id: current_user.id).first
+      @item = Item.find(params[:id])
+      @image = ItemImage.where(item_id: @item.id).first
+      if card.present?
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+      end
   end
 
   def pay
-    # @item = Item.find(params[:id])
-    card = CreditCard.where(user_id: current_user.id )
-    if card.blank?
-      redirect_to action: "new"
-      flash[:alert] = '購入にはクレジットカード登録が必要です'
-    else
       @item = Item.find(params[:id])
-      card = current_user.credit_card
-      # テーブル紐付けてるのでログインユーザーのクレジットカードを引っ張ってくる
+      card = CreditCard.where(user_id: current_user.id).first
+      if card.blank?
+        redirect_to new_credit_card_path 
+      else
       Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       Payjp::Charge.create(
       amount: @item.price, 
       customer: card.customer_id, 
       currency: 'jpy',
       )
-      # ↑商品の金額をamountへ、cardの顧客idをcustomerへ、currencyをjpyへ入れる
-      # @item = Item.new(buyer_id: current_user.id)
-      # if @item.save
-        redirect_to done_items_path 
-      # else
-      #   redirect_to controller: "items", action: 'show'
-      # end
-    end
-
+      end
+        redirect_to done_item_path(@item.id) 
   end
+
   def done
-  end
-
-    
-  def index
-
+    @item = Item.find(params[:id])
+    @item.update(buyer_id: current_user.id)
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def create
@@ -58,11 +47,8 @@ class ItemsController < ApplicationController
   def destroy
   end
 
-  def update
-    @item = Item.find(params[:id])
-  @item.update(buyer_id: current_user.id)
-  end
-
   def new
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :users
   resources :items do
-    collection do
-  get 'buy/:id', to: 'items#buy'
-  post 'pay/:id', to: 'items#pay'
+    member do
+  get 'buy', to: 'items#buy'
+  post 'pay', to: 'items#pay'
   get  'done', to: 'items#done'
     end
   end
-  resources :credit_cards, only: [:new, :show] do
+  resources :credit_cards, only: [:new,:show] do
     collection do
       post 'show', to: 'credit_cards#show'
       post 'pay', to: 'credit_cards#pay'
