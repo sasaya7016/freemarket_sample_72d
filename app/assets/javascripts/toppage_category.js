34c33491517dce12496d1ed32b9a29cfef5971e8
kashimaraiki <raiki3290@gmail.com>
2020-04-10 17:29:25 +0900

    トップページカテゴリー表示完成

--- app/assets/javascripts/toppage_category.js
+++ app/assets/javascripts/toppage_category.js
@@ -6,10 +6,17 @@ $(function(){
     return html;
   }
 
+  function buildGrandChildHTML(child){
+    var html =`
+    <a class="grandchild_category item-link" id="${child.id}"  href="/category/${child.id}">${child.name}</a>
+    `;
+    return html;
+  }
+
   $(document).on("mouseover",".parent-list",function(){
     var id = this.id
     $(".child_category").remove();
-    $(".grand_child_category").remove();
+    $(".grandchild_category").remove();
     $.ajax({
       type: 'get',
       url: 'items/get_toppage_category',
@@ -20,6 +27,26 @@ $(function(){
       children.forEach(function (child){
         var html = buildChildHTML(child);
         $(".children_list").append(html);
+      })
+    });
+  });
+
+  $(document).on("mouseover",".child_category",function(){
+    var id = this.id
+    $(".grandchild_category").remove();
+    $.ajax({
+      type: 'get',
+      url: 'items/get_toppage_category',
+      data: {parent_id: id},
+      dataType: 'json'
+    })
+    .done(function(children){
+      children.forEach(function (child){
+        var html = buildGrandChildHTML(child);
+        $(".grandchildren_list").append(html);
+      })
+      $(document).on("mouseover","child_category",function(){
+        $(".grandchild_category").remove();
       })
     });
   });
