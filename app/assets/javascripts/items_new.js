ddc74157ddc84e3e5258ed41fb810b63e57086ee
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-10 18:41:34 +0900

    Update items_new.js

---
+++ app/assets/javascripts/items_new.js
@@ -0,0 +1,7 @@
+if (document.location.href.match(/\/items\/new/)){
+  document.addEventListener("DOMContentLoaded",()=>{
+    var droparea = document.getElementById('');
+    
+    document.addEventListener("change",)
+  });
+}
