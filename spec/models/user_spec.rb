759f32105ad3c5ef95c5c0c139c16b7789f3168a
mitsumakiNT <mitumakifr@gmail.com>
2020-04-10 16:13:53 +0900

    Create_testcode_for_address_validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,49 +9,55 @@ describe User do
     end
 
     it "nicknameがない場合は登録できない" do
-      user = build(:user, nickname: nil)
+      user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
 
     it "lastnameがない場合は登録できない" do
-      user = build(:user, lastname: nil)
+      user = build(:user, lastname: "")
       user.valid?
       expect(user.errors[:lastname]).to include("can't be blank")
     end
 
     it "firstnameがない場合は登録できない"　do
-      user = build(:user, firstname: nil)
+      user = build(:user, firstname: "")
       user.valid?
       expect(user.errors[:firstname]).to include("can't be blank")
     end
 
     it "lastnameKANAがない場合は登録できない" do
-      user = build(:user, lastnameKANA: nil)
+      user = build(:user, lastnameKANA: "")
       user.valid?
       expect(user.errors[:lastnameKANA]).to include("can't be blank")
     end
 
     it "firstnameKANAがない場合は登録できない"　do
-      user = build(:user, firstnameKANA: nil)
+      user = build(:user, firstnameKANA: "")
       user.valid?
       expect(user.errors[:firstnameKANA]).to include("can't be blank")
     end
 
     it "birth_dateがない場合は登録できない" do
-      user = build(:user, birth_date: nil)
+      user = build(:user, birth_date: ""
       user.valid?
       expect(user.errors[:birth_date]).to include("can't be blank")
     end
 
     it "emailがない場合は登録できない"　do
-      user = build(:user, email: nil)
+      user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
 
+    it "emailに@がない場合は登録できない" do
+      user = build(:user, email:mmmgmail.com)
+      user.valid?
+      expect(user.errors[:email]).to include("is invalid")
+    end
+
     it "passwordがない場合は登録できない"　do
-      user = build(:user, password: nil)
+      user = build(:user, password: "")
       user.valid?
       expect(user.errors[:password]).to include("can't be blank")
     end
