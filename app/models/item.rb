784831237fb24e2688abd2eec5c42891805ec3ba
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-10 15:01:44 +0900

    読みやすいように記述順序を変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,17 +2,16 @@ class Item < ApplicationRecord
   has_many :favorites
   has_many :comments
   has_many :item_images
+  accepts_nested_attributes_for :item_images, allow_destroy: true
   belongs_to :user, optional: true
   belongs_to :category
+  belongs_to :exhibitor, class_name: "User"
   
   #ActiveHashのアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
   include Common_Module
-  belongs_to :exhibitor, class_name: "User"
-
-  accepts_nested_attributes_for :item_images
 
   def previous
     Item.where('id < ?',self.id).order('id DESC').first
