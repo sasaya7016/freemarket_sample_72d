8e24dac525167fe4f68b7f5d97842186e07358d8
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-12 16:12:30 +0900

    画像のHTMLを定義

--- app/assets/javascripts/items_new.js
+++ app/assets/javascripts/items_new.js
@@ -1,4 +1,18 @@
 if (document.location.href.match(/\/items\/new/)){
+    //preview生成
+      //削除ボタン生成
+    const buildImg = (index ,url ) => {
+      const html = `
+      <div data-index="${index}" class="exhibit__image-box__previews__preview">
+        <div class="exhibit__image-box__previews__preview__image">
+          <img class="image${index} input__images" data-index="${index} src="${url}" width="120px" height="120px">
+          <div class="exhibit__image-box__previews__preview__delete image-remove-btn">
+            削除
+          <div>
+        <div>
+      <div>`;
+      return html;
+      }
       
     })
 >>>>>>> Stashed changes
