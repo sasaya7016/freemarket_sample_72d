76562ed10060390d4f041a369342d167c77a8f86
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-12 16:12:01 +0900

    コンフリクトファイルの削除

--- app/assets/javascripts/items_new.js
+++ app/assets/javascripts/items_new.js
@@ -1,11 +1,4 @@
 if (document.location.href.match(/\/items\/new/)){
-  document.addEventListener("DOMContentLoaded",()=>{
-    var droparea = document.getElementById('');
-    
-<<<<<<< Updated upstream
-    document.addEventListener("change",)
-=======
-    document.addEventListener("change",() => {
       
     })
 >>>>>>> Stashed changes
