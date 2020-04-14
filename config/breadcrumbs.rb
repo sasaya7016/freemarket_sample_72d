a6a06d1f02572178096101cddabe81446e084f3a
kashimaraiki <raiki3290@gmail.com>
2020-04-14 13:17:32 +0900

    カテゴリーのパンクズリスト

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,12 +1,29 @@
 crumb :root do
-  link "Home", root_path
+  link "トップページ", root_path
 end
 
-crumb :issues do
-  link "All issues", issues_path
+crumb :category do
+  link "カテゴリー一覧",categories_path
+  parent :root
 end
 
-crumb :issues do |issue|
-  link issue.title, issue
-  parent :issues
+crumb :parent do
+  @category = Category.find(params[:id])
+  if @category.ancestry.nil?
+    link "#{@category.name}",category_path(@category.id)
+    parent :category
+  elsif @category.parent.parent.present?
+    link "#{@category.parent.parent.name}", category_path(@category.parent.parent.id)
+    link "#{@category.parent.name}", category_path(@category.parent.id)
+    link "#{@category.name}",category_path(@category.id)
+    parent :category
+  else
+    link "#{@category.parent.name}", category_path(@category.parent.id)
+    link "#{@category.name}",category_path(@category.parent.id)
+    parent :category
+  end
+end
+
+crumb :list do
+  link "出品", category_new_path
 end
