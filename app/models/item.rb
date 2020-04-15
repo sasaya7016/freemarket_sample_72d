91e844e3ba847437b75c3a16d6d37610e0474aa3
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-15 11:13:17 +0900

    Itemモデルのvalidation
    ・必須入力項目をpresence: true
    ・金額を100〜9999999円までとした

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,5 +12,22 @@ class Item < ApplicationRecord
   include Common_Module
   include ItemsShowMethods
 
+  #出品時のvalidation
+  with_options presence: true do
+    validates :name
+    validates :introduction
+    validates :price
+    validates :category_id
+    validates :status
+    validates :preparation_day
+    validates :exhibitor_id
+    validates :delivery_fee
+    validates :prefecture_id
+  end
+  with_options numericality: {
+    greater_than_or_equal_to: 100,
+    less_than_or_equal_to: 9999999} do
+      validates :price
+    end
 
 end
