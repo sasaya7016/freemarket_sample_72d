01219156d4ea98094424dc253ea3371bbd12fd43
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-15 11:14:49 +0900

    Factory Botのダミーデータ
    ・priceを10000000とした

---
+++ spec/factories/items.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :item_01, class: Item do
+    sequence(:id) { |n| n}
+    sequence(:name) { |n| "TEST_SELLING_#{n}" }
+    sequence(:introduction) { |n| "TEST_INTRODUCTION_#{n}"}
+    sequence(:price) { 10000000 }
+    sequence(:category_id) { 3 }
+    sequence(:status) { "未使用" }
+    sequence(:preparation_day) { 2 }
+    sequence(:exhibitor_id) { 1 }
+    sequence(:delivery_fee) { "着払い（購入者負担）" }
+    sequence(:prefecture_id) { 1 }
+  end
+
