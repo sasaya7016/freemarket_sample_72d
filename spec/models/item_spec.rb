df6cdb9247ce6a201e5bad2ac36b48664634e5f6
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-15 11:18:19 +0900

    必須入力項目があれば商品出品可能なことをテスト
    ・商品出品時にname, introduction, price, category_id, status, preparation_day, exhibitor_id, delivery_fee, prefecture_idがあれば有効であることを確認

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,2 +1,18 @@
 require 'rails_helper'
 
+describe 'presence: true' do
+  it "商品出品時にname, introduction, price, category_id, status, preparation_day, exhibitor_id, delivery_fee, prefecture_idがあれば有効であること" do
+    @item = Item.new(
+      id: 123,
+      name: "music",
+      introduction: "This is the test.",
+      price: 99999,
+      category_id: 3,
+      status: "未使用",
+      preparation_day: 2,
+      exhibitor_id: 1,
+      delivery_fee: "着払い（購入者負担）",
+      prefecture_id: 1,
+    )
+    expect(@item).to be_valid
+  end
