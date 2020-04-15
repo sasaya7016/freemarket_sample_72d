5707ccff2f9d9130f5ffac393b67b14cb10cd3d0
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-15 12:56:41 +0900

    本番環境でのみ画像保存をS3にする

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,11 +4,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  #if Rails.env.production?
+  if Rails.env.production?
     storage :fog
-  #else
-  #  storage :file
-  #end
+  else
+    storage :file
+  end
 
   process convert: 'jpg'
   # Override the directory where uploaded files will be stored.
