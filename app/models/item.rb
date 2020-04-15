f257f6ae16c29afd651421db50092e2cb54c72c8
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-15 11:11:43 +0900

    アソシエーションのオプション追加
    ・belongs_to :exhibitor, class_name: "User"削除
    ・  belongs_to :category, optional: true追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,7 @@ class Item < ApplicationRecord
   has_many :item_images
   accepts_nested_attributes_for :item_images, allow_destroy: true
   belongs_to :user, optional: true
-  belongs_to :category
-  belongs_to :exhibitor, class_name: "User"
+  belongs_to :category, optional: true
   
   #ActiveHashのアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
