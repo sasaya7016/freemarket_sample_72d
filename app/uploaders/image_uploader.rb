c6b6a5782e58c1e12ed78f8340dc7308f89edd6e
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-15 12:20:01 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,9 +4,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  #storage :file
+  storage :fog
 
+  process convert: 'jpg'
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -43,6 +44,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # 変換したファイルのファイル名の規則
   def filename
-    "#{Time.zone.now.strftime('%Y%m%d%H%M')}.png" if original_filename.present?
+    "#{Time.zone.now.strftime('%Y%m%d%H%M')}.jpg" if original_filename.present?
   end
 end
