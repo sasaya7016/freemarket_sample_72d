e563101b427fdc0880de124230e7d58d55259b63
mitsumakiNT <mitumakifr@gmail.com>
2020-04-16 11:39:45 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,15 +10,15 @@ CarrierWave.configure do |config|
     #本番環境のS3アクセスキーを環境変数から取得します
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id:  Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      aws_access_key_id:  Rails.application.credentials.[:aws][:access_key_id],
+      aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
       region: 'ap-northeast-1'
       }
       
 #要入力！
     #本番環境のS3ホストを記入して下さい
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/google-storage'
-    config.fog_directory  = 'google-storage' #本番環境のS3のバケット名
+    config.fog_directory  = 'teamdbucket' #本番環境のS3のバケット名
     config.fog_public = true
     config.fog_provider = 'fog/aws'
     else
