55e41f2349482a79f553879692e28adbdf6e1498
kashimaraiki <raiki3290@gmail.com>
2020-04-17 09:34:54 +0900

    テストコード記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -65,23 +65,88 @@ describe 'presence: true' do
 end
 
 describe '金額の上限、加減' do
-  it "商品出品時、priceが9999999を超えると無効" do
+  it "商品出品、編集時、priceが9999999を超えると無効" do
     @item = build(:item_01)
     expect(@item.valid?).to eq(false)
   end
 
-  it "商品出品時、priceが9999999までは有効" do
+  it "商品出品、編集時、priceが9999999までは有効" do
     @item = build(:item_02)
     expect(@item).to be_valid
   end
 
-  it "商品出品時、priceが100を超えると有効" do
+  it "商品出品、編集時、priceが100を超えると有効" do
     @item = build(:item_03)
     expect(@item).to be_valid
   end
 
-  it "商品出品時、priceが100を下回ると無効" do
+  it "商品出品、編集時、priceが100を下回ると無効" do
     @item = build(:item_04)
     expect(@item.valid?).to eq(false)
   end
+end
+
+
+describe 'presence: true' do
+  it "商品編集時にname, introduction, price, category_id, status, preparation_day, exhibitor_id, delivery_fee, prefecture_idがあれば有効であること" do
+    @item = Item.update(
+      id: 123,
+      name: "music",
+      introduction: "This is the test.",
+      price: 99999,
+      category_id: 3,
+      status: "未使用",
+      preparation_day: 2,
+      exhibitor_id: 1,
+      delivery_fee: "着払い（購入者負担）",
+      prefecture_id: 1,
+    )
+    expect(@item).to be_valid
+  end
+
+  it "商品編集時にnameがなければ無効" do
+    @item = Item.update(name: nil)
+    expect(@item.valid?).to eq(false)
+  end
+
+  it "商品編集時にintroductionがなければ無効" do
+    @item = Item.update(introduction: nil)
+    expect(@item.valid?).to eq(false)
+  end
+
+  it "商品編集時にpriceがなければ無効" do
+    @item = Item.update(price: nil)
+    expect(@item.valid?).to eq(false)
+  end
+
+  it "商品編集時にcategory_idがなければ無効" do
+    @item = Item.update(category_id: nil)
+    expect(@item.valid?).to eq(false)
+  end
+
+  it "商品編集時にstatusがなければ無効" do
+    @item = Item.update(status: nil)
+    expect(@item.valid?).to eq(false)
+  end
+
+  it "商品編集時にpreparation_dayがなければ無効" do
+    @item = Item.update(preparation_day: nil)
+    expect(@item.valid?).to eq(false)
+  end
+
+  it "商品編集時にexhibitor_idがなければ無効" do
+    @item = Item.update(exhibitor_id: nil)
+    expect(@item.valid?).to eq(false)
+  end
+
+  it "商品編集時にdelivery_feeがなければ無効" do
+    @item = Item.update(delivery_fee: nil)
+    expect(@item.valid?).to eq(false)
+  end
+
+  it "商品編集時にprefecture_idがなければ無効" do
+    @item = Item.update(prefecture_id: nil)
+    expect(@item.valid?).to eq(false)
+  end
+
 end
