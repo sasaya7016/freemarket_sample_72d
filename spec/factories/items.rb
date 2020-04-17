4f92be31c5cf17cb5c385f03c85460acddbf1197
kashimaraiki <raiki3290@gmail.com>
2020-04-17 12:25:17 +0900

    テストコード修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -50,4 +50,17 @@ FactoryBot.define do
     sequence(:delivery_fee) { "着払い（購入者負担）" }
     sequence(:prefecture_id) { 1 }
   end
+
+  factory :item_edit, class: Item do
+    sequence(:id) { |n| n}
+    sequence(:name) { |n| "TEST_SELLING_#{n}" }
+    sequence(:introduction) { |n| "TEST_INTRODUCTION_#{n}"}
+    sequence(:price) { 99 }
+    sequence(:category_id) { 3 }
+    sequence(:status) { "未使用" }
+    sequence(:preparation_day) { 2 }
+    sequence(:exhibitor_id) { 1 }
+    sequence(:delivery_fee) { "着払い（購入者負担）" }
+    sequence(:prefecture_id) { 1 }
+  end
 end
