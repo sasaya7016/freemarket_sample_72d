6826f381fa1813d76b2533f1906894e7d600d9bc
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-19 20:54:06 +0900

    サムネイルと画像の解像度変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [400, 400]
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development?
     storage :file
@@ -36,7 +36,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [400, 400]
+    process resize_to_fit: [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
