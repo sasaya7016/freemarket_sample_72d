9832739b887b623e72ac3c583ada8c1047590030
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-19 22:44:33 +0900

    ルーティングを変更
    ・credit_cardはuserに紐づくので、users/:id/credit_card/となるように修正
    ・likesはuserに紐づくのでusers/:id/likesとなるよう修正
    ・support以下のルーティングもusers/:id/support/~　となるよう修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,63 +8,63 @@ Rails.application.routes.draw do
   end
   root 'items#index'
   
-resources :users do
-  member do
-    get 'likes'
-  end
+  resources :users do
     member do
-      resources :credit_cards, only: [:new,:show] do
-        collection do
-          post 'show', to: 'credit_cards#show'
-          post 'pay', to: 'credit_cards#pay'
-          post 'delete', to: 'credit_cards#delete'
+      get 'likes'
+    end
+        resources :credit_cards, only: [:new,:show] do
+          collection do
+            post 'show', to: 'credit_cards#show'
+            post 'pay', to: 'credit_cards#pay'
+            post 'delete', to: 'credit_cards#delete'
+          end
+        end
+      member do
+        get :support
+        get :logout
+        get :address
+        scope :support do
+            get :registration_support
+            get :defect_support
+            get :trouble_support
+            get :request_support
+            get :evaluation_support
+            get :buy_support
+            get :withdraw_support
+            get :other_support
         end
       end
-      get :support
-      get :logout
-      get :address
-      scope :support do
-          get :registration_support
-          get :defect_support
-          get :trouble_support
-          get :request_support
-          get :evaluation_support
-          get :buy_support
-          get :withdraw_support
-          get :other_support
-      end
     end
-  end
-  resources :items do
-    member do
-      get 'buy', to: 'items#buy'
-      post 'pay', to: 'items#pay'
-      get  'done', to: 'items#done'
-      get :buy
-      get :get_category_children, defaults: { format: 'json' }
-      get :get_category_grandchildren, defaults: { format: 'json' }
-      get :get_item_size, defaults: { format: 'json' }
-    end
-    collection do
-      get :get_category_children, defaults: { format: 'json' }
-      get :get_category_grandchildren, defaults: { format: 'json' }
-      get :get_item_size, defaults: { format: 'json' }
-      get :get_item_fee, defaults: { format: 'json' }
-      get :search
-    end
-    resources :favorites do
+    resources :items do
       member do
-        post '/favorite/:item_id', to: 'favorites#create', as: 'like'
-        delete '/favorite/:item_id', to: 'favorites#destroy', as: 'unlike'
+        get 'buy', to: 'items#buy'
+        post 'pay', to: 'items#pay'
+        get  'done', to: 'items#done'
+        get :buy
+        get :get_category_children, defaults: { format: 'json' }
+        get :get_category_grandchildren, defaults: { format: 'json' }
+        get :get_item_size, defaults: { format: 'json' }
+      end
+      collection do
+        get :get_category_children, defaults: { format: 'json' }
+        get :get_category_grandchildren, defaults: { format: 'json' }
+        get :get_item_size, defaults: { format: 'json' }
+        get :get_item_fee, defaults: { format: 'json' }
+        get :search
+      end
+      resources :favorites do
+        member do
+          post '/favorite/:item_id', to: 'favorites#create', as: 'like'
+          delete '/favorite/:item_id', to: 'favorites#destroy', as: 'unlike'
+        end
+      end
+      resources :comments, only: :create
+    end
+    resources :categories do
+      collection do
+        get :get_toppage_category, defaults: { format: 'json' }
+        get :category_scroll, defaults: { format: 'json' }
       end
     end
-    resources :comments, only: :create
+    
   end
-  resources :categories do
-    collection do
-      get :get_toppage_category, defaults: { format: 'json' }
-      get :category_scroll, defaults: { format: 'json' }
-    end
-  end
-  
-end
