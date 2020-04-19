3797e975f4ed413366dd380e1d11120e514b68f2
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-19 22:39:07 +0900

    User関係のbefore_actionを変更
    ・reject_non_authenticate_userの動作アクションにupdateを追加
    ・set_userを全てのアクションで動作するように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   include CommonModuleForControllers
   before_action :authenticate_user!
   before_action :set_item, only: [:edit, :show, :update]
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :reject_non_authenticate_user,only: [:show, :edit]
+  before_action :set_user
+  before_action :reject_non_authenticate_user,only: [:show, :edit, :update]
   before_action :set_category
   before_action :user_params, only: [:update]
   before_action :user_show_info, only: [:show ,:edit ]
@@ -60,6 +60,7 @@ class UsersController < ApplicationController
   end
 
   def likes
+
   end
   
   def user_show_info
