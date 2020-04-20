81744714caa8f5c359e58c607b8c05e8dccae709
kandori-31 <ezynmthd@i.softbank.jp>
2020-04-20 17:02:28 +0900

    udpate items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,7 +32,7 @@ class ItemsController < ApplicationController
       )
     end
     @item.buyer_id = current_user.id
-    if @item.save
+    if @item.save!
       redirect_to root_path, notice: '購入完了しました'
     else
       redirect_to item_path(@item.id), alert: "購入に失敗しました"
