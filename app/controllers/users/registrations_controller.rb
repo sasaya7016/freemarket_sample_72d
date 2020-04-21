95f113e1c6ee545eafb0b2e7fd431afe09f5d02c
kashimaraiki <raiki3290@gmail.com>
2020-04-21 10:52:18 +0900

    追加していたコメントアウト削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,11 +11,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   # def create
-  #   if params[:sns_auth] == 'true'
-  #     pass = Devise.friendly_token
-  #     params[:user][:password] = pass
-  #     params[:user][:password_confirmation] = pass
-  #   end
   #   super
   # end
 
