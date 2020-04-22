e05c0e1ac3a0f313dc63d63625d5de727636063a
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-22 18:41:15 +0900

    商品がSOLDにならない問題を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,7 @@ class Item < ApplicationRecord
   has_many :comments
   has_many :item_images, dependent: :destroy
   
-  # belongs_to :user, optional: true
-  belongs_to :user, foreign_key: "buyer_id",class_name: "User",optional: true
+  belongs_to :user, optional: true
   belongs_to :category, optional: true
   
   #ActiveHashのアソシエーション
