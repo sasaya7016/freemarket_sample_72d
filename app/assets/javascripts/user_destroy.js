ce8488cdff231a859e7aba4acabd6699e5127901
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-22 15:17:55 +0900

    変数の定義とイベントリスナーの登録

--- app/assets/javascripts/user_destroy.js
+++ app/assets/javascripts/user_destroy.js
@@ -1,12 +1,14 @@
 if (document.location.href.match(/\/users\/\d+\/support\/withdraw_support/)){
   document.addEventListener('DOMContentLoaded' , () => {
 
-  let accountDeleteButton = document.getElementById('account-delete-button');
-  let withdrawReasons = document.getElementById('withdraw-reasons');
+    let accountDeleteButton = document.getElementById('account-delete-button');
+    let withdrawReasons = document.getElementById('withdraw-reasons');
 
   let modalBG = document.getElementById('modal__background');
   let modalWindowButtonBack = document.getElementById('back__button')
-  accountDeleteButton.addEventListener( 'click' , deleteAlert, { passive: false } ); 
+    let modalBG = document.getElementById('modal__background');
+    let modalWindowButtonBack = document.getElementById('back__button')
+    accountDeleteButton.addEventListener( 'click' , deleteAlert, { passive: false } ); 
 
   modalWindowButtonBack.addEventListener( 'click' , modalWindowUnlock );
     function deleteAlert(e){
