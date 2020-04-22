5dcec11f416c4b7301b45d0ca5fe4f038032eb2d
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-22 13:28:05 +0900

    DOM要素の取得

---
+++ app/assets/javascripts/user_destroy.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded' , () => {
+
+  let accountDeleteButton = document.getElementById('account-delete-button');
+  let withdrawReasons = document.getElementById('withdraw-reasons');
+
+  let modalBG = document.getElementById('modal__background');
+  let modalWindowButtonBack = document.getElementById('back__button')
