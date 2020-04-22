9b1586c10506ae7910cdb44fbf7a204124a5f993
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-22 15:31:08 +0900

    buyer_idからpurchaser_idに変更

--- db/migrate/20200401083445_create_items.rb
+++ db/migrate/20200401083445_create_items.rb
@@ -11,8 +11,8 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.integer :favorites_count
       t.integer :preparation_day, null: false
       t.integer :exhibitor_id, foreign_key: true
-      t.integer :buyer_id, foreign_key: true
-      t.integer :buyer_id_status
+      t.integer :purchaser_id
+      t.integer :purchaser_id_status
       t.string :delivery_fee, null: false
       t.integer :prefecture_id, null: false, foreign_key: true
       t.timestamps
