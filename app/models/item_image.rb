28d9103622749eadbc5a2b19bd7917fbf6e490f8
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-22 18:29:02 +0900

    items_imagesバリデーション修正

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,6 +1,6 @@
 class ItemImage < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :item
-  validates :item_id, presence: true
+  validates :item, presence: true
   validates :image, presence: true
 end
