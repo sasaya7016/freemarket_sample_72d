e9442d1526a47005b5fb2faab5905316c12bd9e4
kaperrine-dog <kll1moisxharmagedon@gmail.com>
2020-04-22 13:30:21 +0900

    fadeIn関数を定義

--- app/assets/javascripts/user_destroy.js
+++ app/assets/javascripts/user_destroy.js
@@ -33,3 +33,25 @@ document.addEventListener('DOMContentLoaded' , () => {
       modalBG.style.display = 'none';
     }
     
+    function fadeIn(node, duration) {
+      if (getComputedStyle(node).display !== 'none') return;
+      if (node.style.display === 'none') {
+        node.style.display = '';
+      } else {
+        node.style.display = 'block';
+      }
+      node.style.opacity = 0;
+      var start = performance.now();
+      requestAnimationFrame(function tick(timestamp) {
+        // イージング計算式（linear）
+        var easing = (timestamp - start) / duration;
+        node.style.opacity = Math.min(easing, 1);
+        if (easing < 1) {
+          requestAnimationFrame(tick);
+        } else {
+          node.style.opacity = '';
+        }
+      });
+    }
+
+});
